d95861ae430467e9ecdb61255e2a2802adc20197
caroline223 <67647836+caroline223@users.noreply.github.com>
2021-08-10 21:37:14 -0400

    commit tree c42557e157fede2ba26de541ce5b6712976ec37c

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, "Mr.Chocolate", 12, "male", "brown", "nice", 1);
+INSERT INTO bears VALUES (2, "Rowdy", 8, "male", "black", "quiet", 1);
+INSERT INTO bears VALUES (3, "Tabitha", 9, "female", "white", "mean", 0);
+INSERT INTO bears VALUES (4, "Sergeant Brown", 15, "male", "brown", "goofy", 1);
+INSERT INTO bears VALUES (5, "Melissa", 16, "female", "burgendy", "nice", 1);
+INSERT INTO bears VALUES (6, "Grinch", 11, "male", "green", "goofy", 0);
+INSERT INTO bears VALUES (7, "Wendy", 10, "female", "red", "mean", 1);
+INSERT INTO bears VALUES (8, null , 8, "male", "gray", "quiet", 0);
